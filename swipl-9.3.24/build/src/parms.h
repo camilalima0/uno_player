b4151a55462520e832b691e42902db646e79d26c
Camila Claudino de Lima <144016007+camilalima0@users.noreply.github.com>
2025-05-02 23:04:42 +0000

    skdijf

---
+++ swipl-9.3.24/build/src/parms.h
@@ -0,0 +1,21 @@
+/* Generated using cmake/Parms.cmake from src/parms.h.cmake */
+
+#define PLHOME	  "/usr/local/lib/swipl"
+/* #undef PLRELHOME */
+/* #undef PLSHAREDHOME */
+/* #undef PLPKGNAME */
+#define PLARCH            "x86_64-linux"
+#define C_CC              "/usr/bin/cc"
+#define C_CXX             "/usr/bin/c++"
+#define C_CFLAGS          "-fPIC -pthread"
+#define SO_EXT       "so"
+#define SO_PATH      "LD_LIBRARY_PATH"
+/* #undef C_LIBDIR */
+/* #undef C_LIBPLSO */
+#define EXEC_FORMAT  "elf"
+#define LIBPL_PATH   "/usr/local/lib/swipl/lib/x86_64-linux/libswipl.so"
+#define C_LIBS            ""
+#define C_PLLIB           "-lswipl"
+#define C_LDFLAGS         ""
+/* #undef BUILD_MACOS_BUNDLE */
+/* #undef SWIPL_INSTALL_WIN_BUNDLE */
